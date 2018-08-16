82f8a9c48c773f847f8385302776202525e62ee7
Ana Nikolova <ani.nik19@gmail.com>
2018-08-16 11:20:56 +0200

    game

--- js/main.js
+++ js/main.js
@@ -5,42 +5,52 @@ var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 
 var mainBall = new Ball (400,400,20);
+ 
 
 var smallBall1= new Ball (100,100,20); 
 var smallBall2 = new Ball (200,100,20);
 var smallBall3 = new Ball (300,100,20);
 var smallBall4 = new Ball (400,100,20);
 var smallBall5 = new Ball (500,100,20);
+var balls=[smallBall1,smallBall2,smallBall3,smallBall4,smallBall5];
 
 
+//Working formula
 
 function getDistance(x1,y1,x2,y2){
     var xDistance= x2-x1;
     var yDistance= y2-y1;
 
     return Math.sqrt(Math.pow(xDistance,2) + Math.pow(yDistance,2));
-}
-
+} 
 
 setInterval(update, 1000/50);
 
 function update(){
     ctx.clearRect(0,0,width,height);
     mainBall.goToNextPos();
-    if(getDistance(mainBall.x, mainBall.y, smallBall1.x, smallBall1.y)< mainBall.radius + smallBall1.radius){
-    smallBall1.color="aqua";
-    } 
-    checkIfCreate();
-    //crash();
-    //draw
-    mainBall.draw();
-    mainBall.drawArrow();
-    smallBall1.draw();
-    smallBall2.draw();
-    smallBall3.draw();
-    smallBall4.draw();
-    smallBall5.draw();
+    getDistance();
+    //Not working
+    for(var i=0; i<balls.length; i++){  
+        if(getDistance(mainBall.x, mainBall.y, balls[i].x, balls[i].y)< mainBall.radius + balls[i].radius && mainBall.color=== balls[i].color){
+            balls.splice(i,1)
+        }
+    }  
+    
+    //Pythagoras theorie
+    /*if(getDistance(mainBall.x, mainBall.y, smallBall1.x, smallBall1.y)< mainBall.radius + smallBall1.radius){
+        if(mainBall.color===smallBall1.color){
+            smallBall1.color="aqua";  
+    }*/
 
+
+
+    checkIfCreate();
+    mainBall.draw();
+    mainBall.drawArrow(); 
+    for(var i=0; i<balls.length; i++){  
+        balls[i].draw();
+    }  
 }
 
 
@@ -78,13 +88,5 @@ document.onkeydown = function(e) {
     //console.log("mainBall.angle", mainBall.angle);
     
 }
-
-
-  
-
-
-
-  
-  
 
 
